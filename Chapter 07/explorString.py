745f60be8dba81e8b9e46a36e2ef8854bc0a1e8c
Noel Johnson <noel.johnson6213@gmail.com>
2022-08-02 08:36:05 +0530

    Start Chapter 7

---
+++ Chapter 07/explorString.py
@@ -0,0 +1,20 @@
+##GTG
+
+#using single quotes
+sq = 'Hello there'
+
+#using double quotes
+dq = "Hello there"
+
+#using escape sequences
+ess = "Hi\nThere"
+
+#using raw strings: will not consider escape sequences as special characters
+rs = r"Hi\n hi"
+
+#concatenation without the + 
+tw = "Hi" "there"
+
+#consider strings as a sequence of unicode points, and not by byte size
+
+##TYJC
